d33fde05f90723047ff51c7ac72de24d6d11e196
Elorucov <elorucov@live.ru>
2026-01-03 13:58:06 +0400

    Redone XAML-side localization.

---
+++ SmithereenUWP/Converters/LocalizationConverter.cs
@@ -0,0 +1,29 @@
+﻿using SmithereenUWP.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Resources;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace SmithereenUWP.Converters
+{
+    public class LocalizationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (parameter is string key)
+            {
+                return Locale.Get(key);
+            }
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
